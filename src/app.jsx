0c4b4f11f2ae89306880f3d29dfab9ec9ec418f7
Eron Campus <Eron.Jai.Campus@qualicom.com.hk>
2026-01-21 11:50:48 +0800

    Revert

--- src/app.jsx
+++ src/app.jsx
@@ -165,7 +165,7 @@ export default function App() {
                 position: 'absolute',
                 bottom: '0',
                 left: '0',
-                width: '110%',
+                width: '100%',
                 maxHeight: 'calc(100% - 200px)',
                 objectFit: 'contain',
                 objectPosition: 'bottom left',
